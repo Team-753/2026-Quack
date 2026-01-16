f936697bf6c8fa5dbcb7f2881b2013b5625e941e
CeeTortle <christurel741@gmail.com>
2026-01-15 18:22:36 -0800

    WORKS ON TESTBOT YIPPEEEE

--- swerveConfig.py
+++ swerveConfig.py
@@ -1,4 +1,3 @@
-
 # ____           _____ _____ _____    _____ ____  _   _ ______ _____ _____ 
 #|  _ \   /\    / ____|_   _/ ____|  / ____/ __ \| \ | |  ____|_   _/ ____|
 #| |_) | /  \  | (___   | || |      | |   | |  | |  \| | |__    | || |  __ 
@@ -6,10 +5,10 @@
 #| |_) / ____ \ ____) |_| || |____  | |___| |__| | |\  | |     _| || |__| |
 #|____/_/    \_\_____/|_____\_____|  \_____\____/|_| \_|_|    |_____\_____|
 
-driveSpeed=4
-driveTurnSpeed=2
+driveSpeed=1
+driveTurnSpeed=3
 
-driveController="Joystick" #Options:XboxController,Joystick
+driveController="XboxController" #Options:XboxController,Joystick
 driveControllerSlot=0
 
 
@@ -19,21 +18,25 @@ driveControllerSlot=0
 #ROTATION OFFSETS FOR SWERVE ROTATION SENSORS 
     #HOW DO I GET THESE NUMBERS?
     #I MANUALLY SET THE SWERVES TO THE FORWARD POSITION AND THEN PRINTED OUT THE OFFSETS OF EACH ENCODER AND RECORDED THEM
-offsetList=[0.7083120783296617,0.3599781216554734,0.13246701675334055,0.5696525556151847 ] ##quack 0.7083120783296617,0.3599781216554734,0.13246701675334055,0.5696525556151847##
-
+    #0.071533203125,-0.396728515625,0.16455078125,0.45654296875,
+offsetList=[-0.396728515625,0.071533203125,0.16455078125,0.45654296875] ##ONLY APPLIES IF USEING WPI ENCODERS(NOT CANCODER)
+##quack 0.7083120783296617,0.3599781216554734,0.13246701675334055,0.5696525556151847##
+##WIWO 0.7083120783296617,0.3599781216554734,0.13246701675334055,0.5696525556151847## TESTBOT0.45654296875,-0.396728515625,0.071533203125,0.16455078125
 swerveEncoderType="canCoder" #canCoder,wpilibEncoder #IF NOT USING AN ENCODER LEAVE swerveEncoderIds BLANK
+robotCompassType="pidgeon" #navx,pidgeon #NOTE navx not working, need to get inport working
+robotCompassId=13 #FOR PIDGEON ONLY
 
 #SWERVE CAN IDS
-swerveDriveIds=[1,4,7,10]
-swerveTurnIds=[3,6,9,12]
-swerveEncoderIds=[2,5,8,11]
+swerveDriveIds=[4,7,10,1]   #1,4,7,10]
+swerveTurnIds=[6,9,12,3]  # 3,6,9,12]
+swerveEncoderIds=[5,8,0,2]#2,5,8,0] #EXTERNAL ENCODERS, NOT MOTORS
 
 #SWERVE MECHANISM RATIOS
     #HOW TO GET?
     #PRINT OUT MOTOR POSITION AND ROTATE THE MECHANISM ~1FULL ROTATION USE END MOTOR POSITION
     #YOU CAN ALSO GOOGLE IT <-------(THIS IS THE BETTER WAY)
 swerveDriveRatio=(5.68) #NOT NECESSARY FOR ROBOT MOVEMENT, ONLY FOR ACCURATE AUTO #quack 8.14
-swerveTurnRatio=(13.3714)  #NECECCARY IF NOT USING AN ENCODER #quack 12.8
+swerveTurnRatio=(1)  #NECECCARY IF NOT USING AN ENCODER #quack 12.8
 
 #MEASURE FROM POINT WHEEL CONTACTS GROUND
 swerveBaseWidth=(0.64) #METERS
--- swerveSubsys.py
+++ swerveSubsys.py
@@ -1,4 +1,4 @@
-import commands2,phoenix6,wpimath,navx
+import commands2,phoenix6,wpimath
 from math import pi
 import math
 import wpimath.controller
@@ -13,12 +13,10 @@ class swerveSubsys():
         super().__init__()
         self.driveMotor=phoenix6.hardware.TalonFX(driveID)
         self.turnMotor=phoenix6.hardware.TalonFX(turnID)
-        self.turnVariable=self.turnMotor.get_position()
         turnSensorType=swerveConfig.swerveEncoderType
-
         #DRIVE CONFIG
         driveConfig=phoenix6.configs.TalonFXConfiguration()
-        driveConfig.slot0.k_p = 2
+        driveConfig.slot0.k_p = 1
         driveConfig.slot0.k_i = 0.0
         driveConfig.slot0.k_d= 0
         driveConfig.slot0.k_v = 0.01
@@ -27,12 +25,12 @@ class swerveSubsys():
 
         #TURN CONFIG
         turnConfig=phoenix6.configs.TalonFXConfiguration()
-        turnConfig.slot1.k_p = 20
-        turnConfig.slot1.k_i = 0
-        turnConfig.slot1.k_d= 0
+        turnConfig.slot1.k_p = 50
+        turnConfig.slot1.k_i = 0.0001
+        turnConfig.slot1.k_d= 0.1
         turnConfig.slot1.k_v = 0.01
         turnConfig.slot1.k_s = 0.0875
-        #turnConfig.feedback.sensor_to_mechanism_ratio = swerveConfig.swerveTurnRatio
+        turnConfig.feedback.sensor_to_mechanism_ratio = swerveConfig.swerveTurnRatio
         turnConfig.closed_loop_general.continuous_wrap = True
 
         #CONTROL MODES
@@ -49,17 +47,24 @@ class swerveSubsys():
                 self.turnVariable=self.turnSensor.get()
             elif turnSensorType=="canCoder":
                 #CAN CODER CONFIG
-
-                #ADDITIONAL TURN CONFIG
                 self.turnSensor=phoenix6.hardware.CANcoder(turnSensorID)
-                turnConfig.feedback.feedback_remote_sensor_id=turnSensorID
-                turnConfig.feedback.feedback_sensor_source=phoenix6.signals.FeedbackSensorSourceValue.REMOTE_CANCODER        
+                turnSensorConfig=phoenix6.configs.CANcoderConfiguration()
+                turnSensorConfig.magnet_sensor.magnet_offset=swerveConfig.offsetList[swerveConfig.swerveEncoderIds.index(turnSensorID)]
+                turnSensorConfig.magnet_sensor.absolute_sensor_discontinuity_point=0.5 #FACTORY
+                turnSensorConfig.magnet_sensor.sensor_direction=phoenix6.signals.SensorDirectionValue.CLOCKWISE_POSITIVE
+                self.turnSensor.configurator.apply(turnSensorConfig)
+                #ADDITIONAL TURN CONFIG
+                turnConfig.feedback.feedback_sensor_source=phoenix6.signals.FeedbackSensorSourceValue.REMOTE_CANCODER
+                turnConfig.feedback.feedback_remote_sensor_id=turnSensorID 
+                       
         #APPLY CONFIGS
         self.driveMotor.configurator.apply(driveConfig)
         self.turnMotor.configurator.apply(turnConfig)
         if self.hasWpiEnc:
             print("HAS WPIENC")
-            self.turnMotor.set_position(self.turnSensor.get()-swerveConfig.offsetList[turnSensorID])
+            self.turnMotor.set_position(self.turnSensor.get()-swerveConfig.offsetList[swerveConfig.swerveEncoderIds.index(turnSensorID)])
+
+            
     def setState(self,desRot,desSpeed):
         self.turnMotor.set_control(self.postion.with_position(desRot))
         self.driveMotor.set_control(self.dutyCycle.with_velocity(desSpeed))
@@ -70,37 +75,41 @@ class swerveSubsys():
             return self.turnSensor.get_absolute_position().value
     def getState(self):
         return wpimath.kinematics.SwerveModulePosition(self.driveMotor.get_position().value*0.095*pi,wpimath.geometry.Rotation2d(self.turnMotor.get_position().value_as_double*pi*2))
-    def reZero(self,id):
-        self.turnMotor.set_position(self.turnSensor.get()-swerveConfig.offsetList[id])
+    def debug(self):
+        return self.turnSensor.get_position().value
+
 class driveTrainSubsys(commands2.Subsystem):
     def __init__(self):
         super().__init__()
         for i in range(4):
             string=str("self.swerve"+str(i)+"=swerveSubsys("+str(swerveConfig.swerveDriveIds[i])+","+str(swerveConfig.swerveTurnIds[i])+","+str(swerveConfig.swerveEncoderIds[i])+")")
             exec(string)
-        self.navX=navx.AHRS(navx.AHRS.NavXComType.kMXP_SPI)
+        if swerveConfig.robotCompassType=="pidgeon":
+            self.compass=phoenix6.hardware.Pigeon2(swerveConfig.robotCompassId)
+            self.robotRotation=self.compass.getRotation2d()
+        #if swerveConfig.robotCompassType=="navx":
+        #    self.compass=navx.AHRS(navx.AHRS.NavXComType.kMXP_SPI)
+        #    self.robotRotation=self.compass.getRotation2d()
         widthN=swerveConfig.swerveBaseWidth/2
         lengthN=swerveConfig.swerveBaseLength/2
         self.swerveKinematics=wpimath.kinematics.SwerveDrive4Kinematics(wpimath.geometry.Translation2d(widthN/2,lengthN/2),wpimath.geometry.Translation2d(widthN/2,-lengthN/2),wpimath.geometry.Translation2d(-widthN/2,-lengthN/2),wpimath.geometry.Translation2d(-widthN/2,lengthN/2))
 
         #DOES NOT USE GYRO DATA, REPLACE WITH ESTIMATOR
-        #self.poseEstimator=wpimath.estimator.SwerveDrive4PoseEstimator(self.swerveKinematics,wpimath.geometry.Rotation2d.fromDegrees(self.navX.getRotation2d().degrees),self.getSwerveState(),wpimath.geometry.Pose2d(wpimath.geometry.Translation2d(0,0),wpimath.geometry.Rotation2d(0)))
-        self.odometry=wpimath.kinematics.SwerveDrive4Odometry(self.swerveKinematics,wpimath.geometry.Rotation2d.fromDegrees(self.navX.getRotation2d().degrees()),self.getSwerveState(),wpimath.geometry.Pose2d(wpimath.geometry.Translation2d(0,0),wpimath.geometry.Rotation2d(0)))
+        self.odometry=wpimath.kinematics.SwerveDrive4Odometry(self.swerveKinematics,self.robotRotation,self.getSwerveState(),wpimath.geometry.Pose2d(wpimath.geometry.Translation2d(0,0),wpimath.geometry.Rotation2d(0)))
     def setState(self,fb,lr,rot):       
-        #self.swerveNumbers=self.swerveKinematics.toSwerveModuleStates(wpimath.kinematics.ChassisSpeeds(fb,lr,rot))
-        #print(self.swerve0.getRot(),self.swerve1.getRot(),self.swerve2.getRot(),self.swerve3.getRot())
-        self.swerveNumbers=self.swerveKinematics.toSwerveModuleStates(wpimath.kinematics.ChassisSpeeds.fromFieldRelativeSpeeds(fb,lr,rot,wpimath.geometry.Rotation2d.fromDegrees(-self.navX.getRotation2d().degrees())))
-        #print(self.swerveNumbers[0].angle.degrees(),self.swerveNumbers[0].speed_fps)
+        self.swerveNumbers=self.swerveKinematics.toSwerveModuleStates(wpimath.kinematics.ChassisSpeeds(fb,lr,rot))#ROBOT ALIGN)#,self.swerve1.getRot()*360,self.swerve2.getRot()*360,self.swerve3.getRot()*360)
+        #self.swerveNumbers=self.swerveKinematics.toSwerveModuleStates(wpimath.kinematics.ChassisSpeeds.fromFieldRelativeSpeeds(fb,lr,rot,wpimath.geometry.Rotation2d.fromDegrees(-self.navX.getRotation2d().degrees())))#FIELD ALIGN
+        print(self.swerve0.debug(),self.swerve1.debug(),self.swerve2.debug(),self.swerve3.debug())
         for i in range(4):
-            exec(str("self.swerveNumbers["+str(i)+"].optimize(wpimath.geometry.Rotation2d(self.swerve"+str(i)+".getRot()*2*pi))"))
+            #IF JITTERING WITH RIGHT PID, REVERSE OPTIMIZE ANGLE INPUT
+            exec(str("self.swerveNumbers["+str(i)+"].optimize(wpimath.geometry.Rotation2d.fromRotations(self.swerve"+str(i)+".getRot()))"))
             exec(str("self.swerve"+str(i)+".setState(self.swerveNumbers["+str(i)+"].angle.degrees()/360,self.swerveNumbers["+str(i)+"].speed_fps)"))
     def getPoseState(self):
         odo=self.odometry.getPose()
         #print(odo)
         return odo
-        return wpimath.geometry.Pose2d(wpimath.geometry.Translation2d(-odo.x,-odo.y),odo.rotation())
     def periodic(self):
-        self.odometry.update(self.navX.getRotation2d(),self.getSwerveState())
+        self.odometry.update(self.robotRotation,self.getSwerveState())
         return super().periodic()
     def getSwerveState(self):
         string=[]
@@ -133,9 +142,7 @@ class driveTrainCommand(commands2.Command):
        self.addRequirements(driveSubsys,joySubsys)
        self.driveTrain,self.joystick=driveSubsys,joySubsys
     def execute(self):
-        #print(self.joystick.getX(),self.joystick.getY(),self.joystick.getZ())
-              #,self.driveTrain.getSwerveState())
-        self.driveTrain.setState(-self.joystick.getY()*swerveConfig.driveSpeed,self.joystick.getX()*swerveConfig.driveSpeed,-self.joystick.getZ()*swerveConfig.driveTurnSpeed)#self.joystick.getZ()*2)
+        self.driveTrain.setState(self.joystick.getY()*swerveConfig.driveSpeed,self.joystick.getX()*swerveConfig.driveSpeed,-self.joystick.getZ()*swerveConfig.driveTurnSpeed)
 class autoDriveTrainCommand(commands2.Command):
     def __init__(self,driveSubsys:driveTrainSubsys):
         self.addRequirements(driveSubsys)
@@ -144,7 +151,7 @@ class autoDriveTrainCommand(commands2.Command):
         wpigeo=wpimath.geometry
         super().__init__()
         config = wpimath.trajectory.TrajectoryConfig.fromFps(12, 12)
-        config.setReversed(True)
+        #config.setReversed(True)
         #IMPORTANT STUFF
         startPos=wpigeo.Pose2d.fromFeet(0,1,wpigeo.Rotation2d.fromDegrees(180))
         endPos=wpigeo.Pose2d.fromFeet(0,0,wpigeo.Rotation2d.fromDegrees(180))
@@ -153,5 +160,5 @@ class autoDriveTrainCommand(commands2.Command):
     def execute(self):
         self.goal=self.trajectory.sample(1.49)
         speeds=self.holoCont.calculate(self.driveSubsys.getPoseState(),self.goal,wpimath.geometry.Rotation2d(0))
-        print(self.driveSubsys.getPoseState(),self.driveSubsys.getPoseState().y_feet)
+        print(self.driveSubsys.getPoseState())#,self.driveSubsys.getPoseState().y_feet)
         self.driveSubsys.setState(speeds.vx,speeds.vy,speeds.omega)
